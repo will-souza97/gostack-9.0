2bb34e63b17d274120079783327b42868636d9f8
Will <rzy.x97@gmail.com>
2020-09-24 12:53:27 -0300

    Gostack 9.0

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -68,7 +68,7 @@ class UserController {
       return res.status(401).json({ Error: 'Password does not match.' });
     }
 
-    const { id, name, provider } = await User.update(req.body);
+    const { id, name, provider } = await user.update(req.body);
 
     return res.status(201).json({
       id,
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import ProviderControiler from './app/controllers/ProviderControiler';
+import FileController from './app/controllers/FileController';
 import authMiddlewares from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -12,5 +18,9 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderControiler.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
