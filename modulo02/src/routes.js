0a48b58b27c37c691d5abd26a7a27204d961fb56
Will <rzy.x97@gmail.com>
2020-09-24 22:50:52 -0300

    Gostack 9.0

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -21,7 +21,10 @@ routes.use(authMiddlewares);
 
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
