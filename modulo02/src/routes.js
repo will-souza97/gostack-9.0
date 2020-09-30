c6412826e7c4be91da69e27b5d1bd41fa4f78398
Will <rzy.x97@gmail.com>
2020-09-29 23:45:06 -0300

    Gostack 9.0

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -5,9 +5,10 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
-import FileController from './app/controllers/FileController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMiddlewares from './app/middlewares/auth';
 
@@ -22,6 +23,7 @@ routes.use(authMiddlewares);
 routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
+routes.get('/schedule', ScheduleController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 
