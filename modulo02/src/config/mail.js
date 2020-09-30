cd5dc96c186db7c063842eae97a58c5fa013fd05
Will <rzy.x97@gmail.com>
2020-09-30 18:06:53 -0300

    Gostack 9.0

---
+++ modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  auth: {
+    user: 'e7742584f6ee8b',
+    pass: 'dfa41554d58159',
+  },
+  secure: false,
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
